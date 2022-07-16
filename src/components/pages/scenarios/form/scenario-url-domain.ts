b8df0624f5a5aba5673dc06cabfbd6a2faa91d01
h-orito <16453967+h-orito@users.noreply.github.com>
2022-07-16 22:11:00 +0900

    シナリオURLの許可リスト更新

--- src/components/pages/scenarios/form/scenario-url-domain.ts
+++ src/components/pages/scenarios/form/scenario-url-domain.ts
@@ -3,5 +3,6 @@ export const availableDomains = [
   'https://talto.cc/',
   'https://www.pixiv.net/',
   'http://character-sheets.appspot.com/',
+  'https://character-sheets.appspot.com/',
   'https://twitter.com/'
 ]
